ef0e184ac06bd805e16f3382a26efecf77f1dfab
AdhirajGaur09 <yuvrajgaur456@gmail.com>
2026-04-27 02:00:20 +0530

    remove test_gemini.py

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const api = axios.create({ baseURL: '/api' })
+const api = axios.create({ baseURL: 'https://getmeplaced.onrender.com' })
 
 // Attach token from localStorage on every request
 api.interceptors.request.use((config) => {
